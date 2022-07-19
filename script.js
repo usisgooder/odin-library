e7c4db62a720bb370938c4ad0264cb1ed5dd5f2a
Gan Jia Horng <usisgooder@gmail.com>
2022-07-19 19:26:49 +0800

    added book register

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const main = document.querySelector('.main');
-let id = 0;
-let myLibrary = [];
+const overlay = document.querySelector('.overlay');
+const form = document.querySelector('form');
+
+function Book(title, author, year, pages) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+    this.pages = pages;
+}
 
 function getBookDisplay(book) {
     return `
@@ -18,7 +25,7 @@ function getBookDisplay(book) {
                         ${book.pages} ${book.pages > 1 ? "pages": "page"}
                     </div>
                     <div class="card-footer">
-                        <img src="./icons/book-open-variant.svg" alt="" title="Toggle read" class="icon clickable" onclick="toogleRead(this);">
+                        <img src="./icons/book-open-variant.svg" alt="" title="Toggle read" class="icon clickable" onclick="toggleRead(this);">
                         <img src="./icons/trash-can.svg" alt="" title="Delete book" class="icon clickable" onclick="deleteBook(this);">
                     </div>
                 </div>
@@ -38,24 +45,55 @@ function deleteBook(element) {
     element.remove();
 }
 
-function toogleRead(element) {
+function toggleRead(element) {
     element = getCardFromChild(element);
     element.querySelector('.status').classList.toggle('not-read');;
 }
 
-function Book(title, author, year, pages) {
-    this.title = title;
-    this.author = author;
-    this.year = year;
-    this.pages = pages;
+function off() {
+    overlay.style.display = "none";
 }
 
-function addBookToLibrary() {
-    let book = new Book('Test0', 'Goodman', 1970, 297);
+function registerBook() {
+    overlay.style.display = "flex";
+}
+
+function addBookToDisplay(book) {
     main.innerHTML += getBookDisplay(book);
 }
 
+function handleSubmit() {
+    let title = document.getElementById('title').value,
+    author = document.getElementById('author').value,
+    year = document.getElementById('year').value,
+    pages = document.getElementById('pages').value;
+    let book = new Book(title, author, year, pages);
+    addBookToDisplay(book)
+    if (!document.getElementById('completed').checked) {
+        toggleRead(main.lastChild);
+    }
+    off();
+    form.reset();
+}
+
+let randomBooks = [
+    new Book('In Search of Lost Time', 'Marcel Proust', 1913, 4215),
+    new Book('Ulysses', 'James Joyce', 1904, 1920),
+    new Book('Don Quixote', 'Miguel de Cervantes', 1605, 1077),
+    new Book("Alice's Adventures in Wonderland", "Lewis Carroll", 1865, 109),
+    new Book('One Hundred Years of Solitude', 'Gabriel Garcia Marquez', 1967, 401)
+];
 document.querySelector('.fab-container').addEventListener("click", ()=> {
-    console.log('Hii!!!');
-    addBookToLibrary();
+    registerBook();
 });
+document.querySelector('#random').addEventListener('click', () => {
+    addBookToDisplay(randomBooks[Math.floor((Math.random()*randomBooks.length))]);
+    toggleRead(main.lastChild);
+    off();
+    form.reset();
+});
+form.addEventListener('submit', handleSubmit);
+
+addBookToDisplay(
+    new Book("Alice's Adventures in Wonderland", "Lewis Carroll", 1865, 109)
+);
